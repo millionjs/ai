fb784d1a625d1d8e717e10ed64a28444b18d071c
Xinyao <hi@xinyao.me>
2025-03-02 16:36:55 +0800

    feat: patch

--- packages/react/src/use-chat.ts
+++ packages/react/src/use-chat.ts
@@ -254,6 +254,7 @@ By default, it's set to 1, which means that only a single LLM call is made.
       const maxStep = extractMaxToolInvocationStep(
         chatMessages[chatMessages.length - 1]?.toolInvocations,
       );
+      let lastMessage = chatMessages.at(-1)!;
 
       try {
         const abortController = new AbortController();
@@ -343,7 +344,10 @@ By default, it's set to 1, which means that only a single LLM call is made.
             }
           },
           onToolCall,
-          onFinish,
+          onFinish(message, options) {
+            onFinish?.(message, options);
+            lastMessage = message as UIMessage;
+          },
           generateId,
           fetch,
           lastMessage: chatMessages[chatMessages.length - 1],
@@ -378,8 +382,12 @@ By default, it's set to 1, which means that only a single LLM call is made.
           originalMessageCount: messageCount,
           maxSteps,
           messages,
+          lastMessage,
         })
       ) {
+        if (lastMessage) {
+          messages[messages.length - 1] = lastMessage;
+        }
         await triggerRequest({ messages });
       }
     },
